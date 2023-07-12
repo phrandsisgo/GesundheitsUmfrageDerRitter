40d510ecf76144407f3d668f4016d0780d809c44
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-07-12 11:53:14 +0200

    BootstrapExample

--- footer.php
+++ footer.php
@@ -8,7 +8,28 @@
 <body>
     <header>
         <nav>
+
+        </nav>
             
     </header>
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 </body>
 </html>
