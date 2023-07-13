fb72836e0610b85d18aff8201f1295b6f9c72ffc
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-07-13 17:01:19 +0200

    Copilot Daten

---
+++ vorlagen/php-first-2023-07-06/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-primary text-white text-center text-lg-start">
+    <div class="container p-4">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © <?php echo date("d.m.Y"); ?> by Chris
+        </div>
+    </div>
+</footer>
