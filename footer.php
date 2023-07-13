0be79104f490a39f7d6fb3c43ab289be6db05bb5
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-07-13 13:45:51 +0200

    footer am Boden verankert

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+<footer class="footer mt-auto d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
     <p class="col-md-4 mb-0 text-muted">© 2023 EdlenRitter AG</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
