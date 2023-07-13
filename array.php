ce9adde9f84597b8f8da96a9d67b0ea38aa55a51
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-07-13 09:14:05 +0200

    Fragen-Array finished

--- array.php
+++ array.php
@@ -7,13 +7,19 @@ $fragen = array(
     "Hast du das Gefühl, zu wenig, genügend oder viel zu viel zusätzliche körperliche Aktivitäten zu betreiben?"=>"genügend",#hier kommt noch dazu, dass es mehrere Optionen gibt aus der man auswählen muss.
 );
 //hilf mir aus den Fragen ein Multidimensionales Array zu machen.
+//diese Arrays sind folgendermassen geordnet: Frage, Antwort, Typ, Optionen(falls nötig)
  $Fragen = array(
-    array("Wie gesund bist du körperlich?", 5,"1-5"),
+    array("Wie gesund bist du körperlich?", 5,"rangeslider, 1-5"),
     array("Nimmst du Nahrungsergänzungsmittel?", "nein","boolean"),
-    array("Wie wichtig ist köperliche Aktivität für dich?", "5","1-5"),
+    array("Wie wichtig ist köperliche Aktivität für dich?", "5","rangeslider, 1-5"),
     array("Welche zusätzliche körperliche Aktivität betreibst du am meisten?", "Joggen", "multiplechoice",
-        array("Gewichte heben", "gehen", "Joggen", "Radfahren", "Schwimmen", "Tanzen", "Pilates", "Aerobics")),
-    array("Hast du das Gefühl, zu wenig, genügend oder viel zu viel zusätzliche körperliche Aktivitäten zu betreiben?", "genügend"),
-    array()
+        array("Gewichte heben", "gehen", "Joggen", "Radfahren", "Schwimmen", "Tanzen", "Pilates", "Aerobics")),#auf dieser Zeile befindet sich ein Array in einem Array
+    array("Hast du das Gefühl, zu wenig, genügend oder viel zu viel zusätzliche körperliche Aktivitäten zu betreiben?", "genügend", "rangeslider, 1-5"),
+    array("An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Kohlenhydrate?", 1, "eingabefeld Zahl"),
+    array("An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Proteine?", 1, "eingabefeld Zahl"),
+    array("An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Gemüse?", 1, "eingabefeld Zahl"),
+    array("An einem typischen Tag: Wie viele deiner Malzeiten oder Snacks enthalten Früchte?", 1, "eingabefeld Zahl"),
+    array(" An einem typischen Tag: Wie viele deiner Malzeiten kommen aus der Mikrowelle oder sind schon fertig zubereitet?", 1, "eingabefeld Zahl"),
+
  );
 ?>
