59e3cf5e2cc816ccca25f69ba5bcd4805e2a9781
Binyam458 <binitaye14@gmail.com>
2023-07-17 09:49:57 +0200

    footer fixed position fix

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     
-<footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+<footer class="d-flex flex-wrap justify-content-between align-items-center border-top fixed-bottom">
     <p class="col-md-4 mb-0 text-muted">© 2023 EdlenRitter AG</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
